029a61224790954d42eeb76f2aa2aa7df2b4a2d2
Arushi Aloné <137710409+itzarushi16@users.noreply.github.com>
2025-08-20 00:04:23 +0530

    Update DatabaseConnection.java

--- src/hotel_reservation/DatabaseConnection.java
+++ src/hotel_reservation/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_reservation";
     private static final String USER = "username";
-    private static final String PASSWORD = "yourpassword";
+    private static final String PASSWORD = "yourpwd";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
